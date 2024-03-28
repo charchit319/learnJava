364a3caeb72b5ffc6ac060fd396f136de88a0317
charchit <charchitcdahal527@gmail.com>
2024-03-28 13:36:23 +0545

    nothing

--- src/App.java
+++ src/App.java
@@ -1,8 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        
+
         // MainFrame myFrame = new MainFrame();
         // myFrame.init();
+
+        // variables in JAVA
+        
+        int x;
+
+
+
     }
 }
